e8eab0ad0434e5822fa87b503e8932e46b926fee
Ankit Budhathoki <ankitbc777@gmail.com>
2025-02-12 02:38:40 +0545

    add more section and make responsive

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+$('.service-preview').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
